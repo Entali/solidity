9f326f9a2f0bbefc37b221555a06c71b7cc0d245
Nata Entali <nata.entali@protonmail.com>
2024-03-12 00:47:15 +0100

    deploy SimpleStorage

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,14 +1,23 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
 require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+
 
 module.exports = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY]
     },
   },
-  // the rest of your config goes here
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
 };
+
+
