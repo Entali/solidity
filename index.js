a79b963ad153c10fbb0427c675f8d854505eaf27
Nata Entali <nata.entali@protonmail.com>
2024-03-11 17:34:31 +0100

    HDNodeWallet

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import { ethers } from 'ethers'
 const mnemonic12 = ethers.Wallet.createRandom().mnemonic.phrase
 console.log('mnemonic12', mnemonic12)
 
-const mnemonic32 = ethers.utils.entropyToMnemonic(ethers.utils.randomBytes(32))
-console.log('mnemonic32', mnemonic32)
+const mnemonic24 = ethers.utils.entropyToMnemonic(ethers.utils.randomBytes(32))
+console.log('mnemonic24', mnemonic24)
 
