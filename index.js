3aa21a6ac18be7a19d350048c0b7b78f3074616d
Nata Entali <nata.entali@protonmail.com>
2024-03-11 13:36:25 +0100

    mnemonic12, mnemonic24

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import { ethers } from 'ethers'
 
-const randomMnemonic = ethers.utils.entropyToMnemonic(ethers.utils.randomBytes(16))
-console.log('randomMnemonic', randomMnemonic)
+// Генеруємо seed-фразу
+const mnemonic12 = ethers.Wallet.createRandom().mnemonic.phrase
+console.log('mnemonic12', mnemonic12)
+
+const mnemonic32 = ethers.utils.entropyToMnemonic(ethers.utils.randomBytes(32))
+console.log('mnemonic32', mnemonic32)
+
