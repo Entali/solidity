1ff85e0bb44a91ff2bc7e8e6a9c0e9588ff1c1a3
Anastasiia Bortniichuk <nata.bortniichuk@kyivstar.net>
2024-03-12 22:19:37 +0100

    wallet updated

--- wallet.js
+++ wallet.js
@@ -1,9 +1,9 @@
-import { HDNodeWallet } from 'ethers'
+require('dotenv').config()
+const { HDNodeWallet } = require('ethers')
 
 // Generating seed phrase
-const mnemonic = HDNodeWallet.createRandom().mnemonic.phrase
+const mnemonic = process.env.MNEMONIC || HDNodeWallet.createRandom().mnemonic.phrase
 console.log('mnemonic::: ', mnemonic)
-
 
 // Creating wallet
 const wallet = HDNodeWallet.fromPhrase(mnemonic)
@@ -19,6 +19,7 @@ async function sign () {
 
 void sign()
 
-// public address::: 0x61E2060204CEc3f605b27F27F7E430E2399f14Bd
-// signature:::  0xc2cf6dc65b9620515c60bb89e01f0df86ab053464ff28dd1ed579e2ff09fac5e0b23edf11eceae9d40c4d76ef64843d1bada485593dc750cc554c964f0b1c1021b
+
+
+
 
