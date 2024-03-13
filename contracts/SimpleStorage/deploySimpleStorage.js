29edde80a80716cf0d906bc22aba8f60fa061836
Nata Entali <nata.entali@protonmail.com>
2024-03-13 10:13:19 +0100

    deploy script updated

--- contracts/SimpleStorage/deploySimpleStorage.js
+++ contracts/SimpleStorage/deploySimpleStorage.js
@@ -1,11 +1,17 @@
-module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
+const hre = require('hardhat');
 
-  await deploy("SimpleStorage", {
-    from: deployer,
-    args: [],
-    log: true,
-  });
-};
-module.exports.tags = ["SimpleStorage"];
+async function main() {
+  const SimpleStorage = await hre.ethers.getContractFactory("SimpleStorage");
+  const simpleStorage = await SimpleStorage.deploy()
+
+  await simpleStorage.waitForDeployment();
+
+  console.log(`SimpleStorage deployed to ${simpleStorage.target}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+// Contract address::: 0x0a117D67AD46D1CC173D416148Aa396e87D44fA9
